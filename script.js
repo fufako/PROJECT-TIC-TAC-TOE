bee1571d431a599b431a3efe26b541086fa84100
fufako <patryko.mikiciuk@gmail.com>
2022-07-12 14:40:00 +0200

    Add displayController and gameBoard objects, setup setBoardField func

--- script.js
+++ script.js
@@ -5,8 +5,44 @@ const makeBoard = (() => {
       let gridItem = document.createElement("div")
       gridItem.dataset.row = i
       gridItem.dataset.column = j
+      gridItem.className = "field"
       gridItem.innerHTML = "row: " + i + ", column: " + j
       board.appendChild(gridItem)
     }
   }
+})()
+
+const gameBoard = (() => {
+  const board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ]
+
+  const setBoardField = (field, sign) => {
+    board[field.dataset.row][field.dataset.column] = sign
+    field.textContent = sign
+  }
+
+  return { setBoardField }
+})()
+
+const displayController = (() => {
+  const fields = document.querySelectorAll(".field")
+  fields.forEach((field) =>
+    field.addEventListener("click", (e) => {
+      if (gameStatus.isOver() || e.target.textContent === "") {
+        return
+      }
+      gameBoard.setBoardField(field, "X")
+    })
+  )
+})()
+
+const gameStatus = (() => {
+  const isOver = () => {
+    return false
+  }
+
+  return { isOver }
 })()
