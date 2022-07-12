7d061d794239e53e3f3253b53a2e4fb3bd507d67
fufako <patryko.mikiciuk@gmail.com>
2022-07-12 15:19:50 +0200

    Add Player object, update gameStatus, clicks work, appending to array works, X/O changing

--- script.js
+++ script.js
@@ -12,6 +12,15 @@ const makeBoard = (() => {
   }
 })()
 
+const Player = (sign) => {
+  this.sign = sign
+  const getSign = () => {
+    return sign
+  }
+
+  return { getSign }
+}
+
 const gameBoard = (() => {
   const board = [
     ["", "", ""],
@@ -22,27 +31,50 @@ const gameBoard = (() => {
   const setBoardField = (field, sign) => {
     board[field.dataset.row][field.dataset.column] = sign
     field.textContent = sign
+    console.log(board)
+  }
+  const resetBoard = () => {
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        board[i][j] = ""
+      }
+    }
   }
 
-  return { setBoardField }
+  return { setBoardField, resetBoard }
 })()
 
 const displayController = (() => {
   const fields = document.querySelectorAll(".field")
+  const resetBtn = document.getElementById("reset")
   fields.forEach((field) =>
     field.addEventListener("click", (e) => {
       if (gameStatus.isOver() || e.target.textContent === "") {
         return
       }
-      gameBoard.setBoardField(field, "X")
+      gameStatus.playRound(field)
     })
   )
+  resetBtn.addEventListener("click", (e) => {
+    gameBoard.resetBoard()
+  })
 })()
 
 const gameStatus = (() => {
+  const playerX = Player("X")
+  const playerO = Player("O")
+  let move = 1
+
+  const playRound = (field) => {
+    gameBoard.setBoardField(field, getCurrentPlayer())
+    move += 1
+  }
+  const getCurrentPlayer = () => {
+    return move % 2 === 1 ? playerX.getSign() : playerO.getSign()
+  }
   const isOver = () => {
     return false
   }
 
-  return { isOver }
+  return { isOver, playRound }
 })()
